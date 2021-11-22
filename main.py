cf3b49b3dce9b02f49ce6237807f366ce200f60e
paulinadujavova <paulina.dujavova@gmail.com>
2021-11-22 16:09:45 +0100

    oprava prazdnych riadkov

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pygame
 from pygame.locals import *
 
 
-
 TILE_SIZE = 40
 WINDOWWIDTH = 600
 WINDOWHEIGHT = 520
@@ -12,12 +11,12 @@ DISPLAY_SURFACE = None
 TILE_COLOR = "black"
 BG_COLOR = "white"
 
+
 class Tile:
     def __init__(self, x, y, tile_type):
         self.x = x
         self.y = y
         self.type = tile_type
-
     
     def __str__(self):
         return self.type
@@ -40,11 +39,13 @@ def terminate():
     pygame.quit()
     sys.exit()
 
+
 def get_left_top_of_tile(board, tile):
     x, y = tile.x, tile.y
     left = x * (TILE_SIZE + 1) + board.x_margin 
     right = y * (TILE_SIZE + 1) + board.y_margin 
     return left, right
+
 
 def draw_tile(board, tile):
     x_tile, y_tile = get_left_top_of_tile(board, tile)
@@ -59,8 +60,6 @@ def draw_board(board):
             draw_tile(board, Tile(index_j, index_i, None))
 
 
-
-
 def main():
     global DISPLAY_SURFACE
     
@@ -68,17 +67,12 @@ def main():
     DISPLAY_SURFACE = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     game_board = Board(10,10)
     game_board.generate()
-
-    
     while True:
         pygame.display.update()
-
         draw_board(game_board)
-
         for event in pygame.event.get():
             if event.type == QUIT:
                 terminate()
 
 if __name__ == '__main__':
     main()
-
